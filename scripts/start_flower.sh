453b6f8d6c0cf3c04c62f26ca84a2ca43753e919
Vlad <vladimir.prosser@retargetly.com>
2020-10-04 04:10:33 -0300

    cambiar pass

--- scripts/start_flower.sh
+++ scripts/start_flower.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-flower  --address=0.0.0.0 --port=5555 --broker=redis://${REDIS_USER}:${REDIS_PASSWORD}:6379/0
+flower  --address=0.0.0.0 --port=5555 --broker=redis://:${REDIS_PASSWORD}@redis:6379/0
